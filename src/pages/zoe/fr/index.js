3ebfb4ea6104e96bb7ca753e9d3f0c5c33405a1e
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-12-15 11:01:34 -0500

    Fixed import

--- src/pages/zoe/fr/index.js
+++ src/pages/zoe/fr/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../../../components/Layout'
 
 export default class IndexPage extends React.Component {
   render() {
